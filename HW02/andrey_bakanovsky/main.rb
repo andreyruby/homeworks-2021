55a668c2947274557c258ddf1552b20e3417eb93
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-04-25 21:18:14 +0300

    Update module name and filename

--- HW02/andrey_bakanovsky/main.rb
+++ HW02/andrey_bakanovsky/main.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'hw02'
-using Enumerable_Refine
+require_relative 'array_refine'
+using ArrayRefine
 puts a = [1, 2, 3, 4]
 puts 'my_map:'
 b = a.my_map { |x| x**3 }
 puts b
 puts 'my_select:'
 puts a.my_select(&:even?)
-puts a.select { |x| x.even? }
 puts 'my_each:'
 a.my_each { |y| puts y * 2 }
