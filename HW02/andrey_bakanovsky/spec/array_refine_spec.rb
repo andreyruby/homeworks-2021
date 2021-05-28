2a45d2667b5f85f05b6d777ee67fcf8c313c9154
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-05-28 21:48:10 +0300

    HW05 Tests of HW02 changed according comments

--- HW02/andrey_bakanovsky/spec/array_refine_spec.rb
+++ HW02/andrey_bakanovsky/spec/array_refine_spec.rb
@@ -8,27 +8,28 @@ RSpec.describe ArrayRefine, Array do
   subject(:test_array) { [11, 20, 93, 4, -5] }
 
   describe '#my_map' do
-    context 'when calls for each element' do
-      it 'returns the original element to a new array' do
-        new_array = test_array.my_map { |x| x }
-        expect(new_array).to match_array(test_array)
-      end
+    context 'when calls for array' do
+      let(:result_array) { [12, 21, 94, 5, -4] }
 
-      it 'returns new element to a new array' do
+      it 'returns a new array' do
         new_array = test_array.my_map { |x| x + 1 }
-        expect(new_array).to match_array([12, 21, 94, 5, -4])
+        expect(new_array).to match_array(result_array)
       end
 
-      it 'returns each element to rise to the therd power' do
-        new_array = test_array.my_map { |x| x**3 }
-        true_array = test_array.map { |x| x**3 }
-        expect(new_array).to match_array(true_array)
+      it 'don\'t modify original array' do
+        expect(test_array.my_map { |x| x**3 }.object_id) != test_array.object_id
       end
+    end
 
-      it 'save original element' do
-        original_array = [11, 20, 93, 4, -5]
-        test_array.my_map { |x| x**3 }
-        expect(test_array).to match_array(original_array)
+    context 'when calls for empty array' do
+      it 'returns empty array' do
+        expect([].my_map { |x| x }).to be_empty
+      end
+    end
+
+    context 'when calls without block' do
+      it 'returns empty array' do
+        expect(test_array.my_map).to be_empty
       end
     end
   end
@@ -47,23 +48,52 @@ RSpec.describe ArrayRefine, Array do
         expect(test_array.my_select { |x| x < -5 }).to be_empty
       end
     end
+
+    context 'when calls for empty array' do
+      it 'returns empty array' do
+        expect([].my_select { |x| x < 1.14 }).to be_empty
+      end
+    end
+
+    context 'when calls without block' do
+      it 'returns empty array' do
+        expect(test_array.my_select).to be_empty
+      end
+    end
+
+    context 'when calls for array' do
+      it 'don\'t modify original array' do
+        expect(test_array.my_select { |x| x > 32 }.object_id) != test_array.object_id
+      end
+    end
   end
 
   describe '#my_each' do
-    context 'when calls for each element' do
-      it 'returns the original element to a new array' do
+    context 'when calls for array' do
+      it 'returns a new array' do
         new_array = test_array.my_each { |x| x }
         expect(new_array).to match_array(test_array)
+      end
+
+      it 'don\'t modify original array' do
+        expect(test_array.my_each { |x| x**3 }.object_id) != test_array.object_id
       end
 
       it 'does not return new elements after operation in the block' do
         new_array = test_array.my_each { |x| x * 0.314 }
         expect(new_array).to match_array(test_array)
       end
+    end
 
-      it 'save original elements after operation in the block' do
-        original_array = [11, 20, 93, 4, -5]
-        expect(original_array.my_each { |x| x**3 }).to match_array(test_array)
+    context 'when calls without block' do
+      it 'returns original array' do
+        expect(test_array.my_each).to match_array(test_array)
+      end
+    end
+
+    context 'when calls for empty array' do
+      it 'returns empty array' do
+        expect([].my_each { |x| x }).to be_empty
       end
     end
   end
